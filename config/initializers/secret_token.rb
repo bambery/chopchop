92686f1cf3547e0a5ba323b4c4e92a29a2c52067
lwszolek <lwszolek@gmail.com>
2012-09-29 10:05:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chopchop::Application.config.secret_token = '132c83a2411c514599cb534d52b752047765c8df3b10e3ef1ff6dc91d6a90ab9029f080b6243a26f176fdee7f3be5dba77fa4c39539ba5c5c3049d9eaceb31ae'
