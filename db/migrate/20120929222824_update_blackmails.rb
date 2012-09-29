d162ac5b5a315aeca3e8d8afa3552efb501a5c7b
lwszolek <lwszolek@gmail.com>
2012-09-29 18:10:41 -0500

    misnamed a column on blackmails

--- db/migrate/20120929222824_update_blackmails.rb
+++ db/migrate/20120929222824_update_blackmails.rb
@@ -1,6 +1,6 @@
 class UpdateBlackmails < ActiveRecord::Migration
   def change
-    rename_column :blackmails, :sender_id, :hustler
+    rename_column :blackmails, :sender_id, :hustler_id
     rename_column :blackmails, :recipient_id, :victim_id
     add_column :blackmails, :amount, :decimal
     add_column :blackmails, :status_cd, :integer 
