36f8a47b2594b6f1a1fc662ddaeea05b00972ca8
lwszolek <lwszolek@gmail.com>
2012-09-29 17:48:52 -0500

    updating blackmails model

--- app/models/blackmail.rb
+++ app/models/blackmail.rb
@@ -1,5 +1,17 @@
 class Blackmail < ActiveRecord::Base
-  belongs_to :sender
-  belongs_to :recipient
+  validate :hustler_is_not_victim
+
+  belongs_to  :hustler,
+              :foreign_key => :hustler_id,
+              :class_name => 'User'
+  belongs_to  :victim, 
+              :foreign_key => :victim_id,
+              :class_name => 'User'
   attr_accessible :deadline, :note, :video_url
+
+  def huslter_is_not_victim
+    if hustler.email == victim.email
+      errors.add(:victim_id, "You can't blackmail yourself.")
+    end
+  end
 end
