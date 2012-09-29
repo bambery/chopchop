d02286ac65e3428774ac21214bdd5d33e65c6c1e
Sandro Padin <sandropadin@gmail.com>
2012-09-29 16:24:54 -0500

    * adding auth routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Chopchop::Application.routes.draw do
+  match "auth/:service"          => "auth#service"
+  match "auth/:service/callback" => "auth#callback"
+  match "logout"                 => "auth#logout"
+  
   root :to => 'welcome#index'
 end
