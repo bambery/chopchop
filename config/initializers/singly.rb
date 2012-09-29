86aa5a5909494beaedcdea6f6517c4e16a002325
Sandro Padin <sandropadin@gmail.com>
2012-09-29 16:24:44 -0500

    * moving singly.rb to initializers.

--- config/initializers/singly.rb
+++ config/initializers/singly.rb
@@ -1,5 +1,5 @@
 singly = YAML.load_file("config/singly.yml")
 
-Rails.application.config.middleware.use OmniAuth::Builder do
+Chopchop::Application.config.middleware.use OmniAuth::Builder do
   provider :singly, singly["client_id"], singly["client_secret"]
 end
