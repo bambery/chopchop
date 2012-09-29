f050b3b4aed1d4d404e15223e58cb097f260ef11
Sandro Padin <sandropadin@gmail.com>
2012-09-29 16:25:04 -0500

    * adding generated files.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,21 @@
+class WelcomeController < ApplicationController
+  def index
+    if access_token
+      @profiles = HTTParty.get(profiles_url,
+        :query => {:access_token => access_token}
+      ).parsed_response
+    end
+  end
+
+private
+
+  def access_token
+    session[:access_token]
+  end
+
+  SINGLY_API_BASE = "https://api.singly.com"
+
+  def profiles_url
+    "#{SINGLY_API_BASE}/profiles"
+  end
+end
